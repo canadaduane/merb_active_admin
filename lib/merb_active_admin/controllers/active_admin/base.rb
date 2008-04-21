530ebd594899daba31e90333c54050b986d0d4b4
Duane Johnson <duane.johnson@gmail.com>
2008-04-20 22:54:02 -0600

    Don't show ID columns

--- lib/merb_active_admin/controllers/active_admin/base.rb
+++ lib/merb_active_admin/controllers/active_admin/base.rb
@@ -90,6 +90,10 @@ module ActiveAdmin
       underscored_to_human_readable( word.underscore )
     end
     
+    def non_id_columns(model)
+      model.columns.select{ |col| col.to_s !~ /_id$/ }
+    end
+    
     def column_type(model, column)
       model.schema.create_info.first.find{ |c| c[:name] == column.to_sym }[:type]
     end
